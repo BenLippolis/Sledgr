1038870528441ebeb37352ff0aeafa1d2182da11
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-11-29 00:28:00 -0500

    Added models folder, mongoose, and mongodb

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,24 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 // Create new instance of Google Passport Strategy
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    // User already exists
+                } else {
+                    // Create new user 
+                    new User({googleId: profile.id }).save();
+                }
+            })
     })
 );
