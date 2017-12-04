38280ee380a77889e772643762a6eb82e7abd6f8
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-12-03 19:33:17 -0500

    Add client production build config

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -26,20 +27,23 @@ class Header extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Sledgr</a>
+                <Link to={this.props.auth ? '/' : '/'} className="navbar-brand">
+                    Sledgr
+                </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
                 <ul className="nav navbar-nav navbar-right">
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
-                    </li>
+
                 </ul>
                 <ul className="nav navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Features</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Pricing</a>
+                    </li>
                     <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Dropdown link
--- index.js
+++ index.js
@@ -22,6 +22,18 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+// Config for production routing with React Router 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve production assets like main js & css 
+    app.use(express.static('client/build'));
+
+    // Express will serve index.html if it does not recognize route 
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
