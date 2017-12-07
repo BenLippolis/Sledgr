1214e87bdd9d06014cc561b6a85a1a95ac5a79d6
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-12-06 23:19:11 -0500

    Add initial survey form review functionality

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SurveyFormReview extends Component {
-    render() {
-        return (    
-            <div> Survey Form Review </div>
-        );
-    }
-}
+const SurveyFormReview = ({ onCancel }) => {
+    return (    
+        <div> 
+            <h3> Survey Form Review </h3>
+            <button 
+                className="btn btn-danger"
+                onClick={onCancel}
+            >
+            Back
+            </button>
+        </div>
+    );
+};
 
 export default SurveyFormReview;
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    // Component level state for review component
+    // Utilize bable to avoid writing constructor
+    state = { showFormReview: false };
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return (
+                <SurveyFormReview
+                    onCancel={() => this.setState({ showFormReview: false})}
+                />
+            );
+        } 
+        return (
+            <div>
+            <h3> New Survey </h3> 
+            <SurveyForm 
+                // Callback updates state upon form submission
+                onSurveySubmit={() => this.setState({ showFormReview: true })}
+            />
+            </div>
+        );
+    }
+
     render() {
         return (    
             <div>
-                <h2> New Survey </h2> 
-                <SurveyForm />   
+                {this.renderContent()}
             </div>
 
         );
