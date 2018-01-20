8b94ed529ea6cb7e4006d8bf755293b301303b0e
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2018-01-20 15:36:20 -0500

    Update env variables for production deploy

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,9 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  plaidClientId: process.env.PLAID_CLIENT,
+  plaidSecret: process.emv.PLAID_SECRET,
+  plaidPublic: process.env.PLAID_PUBLIC,
+  plaidEnv: process.env.PLAID_ENVIRONMENT
 }
