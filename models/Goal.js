508caa8db3a6e931cc13f7314afb92b365c2b19f
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2018-02-23 15:47:03 -0500

    Update Goal model

--- models/Goal.js
+++ models/Goal.js
@@ -4,14 +4,15 @@ const { Schema } = mongoose
 const weekSchema = new mongoose.Schema({
   time: { type: Date, default: Date.now },
   active: { type: Boolean, default: true },
-  max_spend: Number,
-  end_spend: Number
+  max_spend: { type: Number, default: 0 },
+  end_spend: { type: Number, default: 0 },
+  success: Boolean
 })
 
 const goalSchema = new Schema({
   time: { type: Date, default: Date.now },
   active: { type: Boolean, default: true },
-  week_count: Number,
+  week_count: { type: Number, default: 0 },
   weeks: [weekSchema],
   _user: { type: Schema.Types.ObjectId, ref: 'User' }
 })
