06463407edca62f950d55e0eb1d8da8aa7ca9f5c
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2018-02-27 22:14:42 -0500

    Clean up plaid routes and add react-fontawesome

--- routes/plaidRoutes.js
+++ routes/plaidRoutes.js
@@ -8,25 +8,25 @@ const Goal = mongoose.model('goal')
 const Week = mongoose.model('goal')
 const Outflow = mongoose.model('outflow')
 
-const PLAID_CLIENT_ID = keys.plaidClientId
-const PLAID_SECRET = keys.plaidSecret
-const PLAID_PUBLIC_KEY = keys.plaidPublic
-const PLAID_ENV = keys.plaidEnv
+const plaidClientId = keys.plaidClientId
+const plaidSecret = keys.plaidSecret
+const plaidPublic = keys.plaidPublic
+const plaidEnv = keys.plaidEnv
 
 // Initialize Plaid client
 var client = new plaid.Client(
-  PLAID_CLIENT_ID,
-  PLAID_SECRET,
-  PLAID_PUBLIC_KEY,
-  plaid.environments[PLAID_ENV]
+  plaidClientId,
+  plaidSecret,
+  plaidPublic,
+  plaid.environments[plaidEnv]
 )
 
 module.exports = app => {
   // Get access token from Plaid
   app.post('/api/get_access_token', requireLogin, async (req, res, next) => {
-    PUBLIC_TOKEN = req.body.public_token
+    publicToken = req.body.public_token
     await client.exchangePublicToken(
-      PUBLIC_TOKEN,
+      publicToken,
       async (error, tokenResponse) => {
         if (error != null) {
           var msg = 'Could not exchange public_token!'
@@ -35,17 +35,17 @@ module.exports = app => {
             error: msg
           })
         }
-        ACCESS_TOKEN = tokenResponse.access_token
-        ITEM_ID = tokenResponse.item_id
-        console.log('Access Token: ' + ACCESS_TOKEN)
-        console.log('Item ID: ' + ITEM_ID)
+        accessToken = tokenResponse.access_token
+        itemId = tokenResponse.item_id
+        console.log('Access Token: ' + accessToken)
+        console.log('Item ID: ' + itemId)
         res.json({
           error: false
         })
         const user = req.user
         if (!user.accessToken) {
-          user.accessToken = ACCESS_TOKEN
-          user.itemId = ITEM_ID
+          user.accessToken = accessToken
+          user.itemId = itemId
           await user.save()
 
           // Update profile stage
@@ -64,12 +64,12 @@ module.exports = app => {
   app.get('/api/transactions', requireLogin, async (req, res, next) => {
     // Find active week
     // Use week.time to pull necesary transactions
-    const active_goal = await Goal.findOne({
+    const activeGoal = await Goal.findOne({
       _user: req.user.id,
       active: true
     })
-    const active_week = active_goal.weeks.find(week => week.active === true)
-    console.log(active_week)
+    const activeWeek = activeGoal.weeks.find(week => week.active === true)
+    console.log(activeWeek)
 
     var startDate = moment().subtract(2, 'days').format('YYYY-MM-DD')
     var endDate = moment().format('YYYY-MM-DD')
@@ -101,23 +101,6 @@ module.exports = app => {
         res.json(displayTxns)
       }
     )
-  })
-
-  // Fetch the users account balance
-  app.get('/api/balance', requireLogin, function (req, res) {
-    client.getAccounts(req.user.accessToken, function (
-      error,
-      transactionsResponse
-    ) {
-      if (error != null) {
-        console.log(error)
-        return res.json({ error: error })
-      }
-      console.log(transactionsResponse.accounts[0].balances.available)
-      // By default we're fetching the first account (temp)
-      // This should fetch the one the user selects
-      res.json(transactionsResponse.accounts[0].balances.available)
-    })
   })
 
   // Only available in production :(
