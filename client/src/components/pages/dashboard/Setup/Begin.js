990a49f9771d6719d7bebe5fca05d9c36dd3d86e
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2018-03-02 11:44:41 -0500

    Test bb conection

--- client/src/components/pages/dashboard/Setup/Begin.js
+++ client/src/components/pages/dashboard/Setup/Begin.js
@@ -28,7 +28,7 @@ class Begin extends Component {
             )}
           </b>
           {' '}
-          on extras per week and still save <b>
+          on extras per week and still save test <b>
             $
             {roundTo(this.props.profile.weeklyTargetSavings, 0)}
           </b>
